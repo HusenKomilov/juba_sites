2fafe31584900e1e600856a7f99c1650c0bc9c9f
HusenKomilov <husenkomilov73@gmail.com>
2024-10-20 13:54:01 +0500

    feat: rest_framework setttings

--- config/settings.py
+++ config/settings.py
@@ -133,4 +133,11 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
