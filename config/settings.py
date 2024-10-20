964c1abc85e21835c5f95cb7653d7a453ba280c5
HusenKomilov <husenkomilov73@gmail.com>
2024-10-20 13:46:02 +0500

    feat: api urls

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+CKEDITOR_UPLOAD_PATH = "uploads/"
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-root
+MEDIA_ROOT = str(BASE_DIR / "media")
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
