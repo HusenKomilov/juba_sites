62ff58e4f0d0176951e54cd2065fec05127523b1
HusenKomilov <husenkomilov73@gmail.com>
2024-10-26 16:25:46 +0500

    fix: cors

--- config/settings.py
+++ config/settings.py
@@ -155,6 +155,12 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "OPTIONS"
+]
---
+++ juba/translation.py
@@ -0,0 +1,6 @@
+from modeltranslation.translator import register, TranslationOptions
+from juba import models
+
+
+# @register(models.Branch)
+# class BranchTranslationOptions(TranslationOptions):
