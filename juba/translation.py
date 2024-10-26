449ae164b74c64117eb242e72c8d69595ae9d46e
HusenKomilov <husenkomilov73@gmail.com>
2024-10-26 16:53:57 +0500

    feat: translation

--- juba/translation.py
+++ juba/translation.py
@@ -1,6 +1,27 @@
 from modeltranslation.translator import register, TranslationOptions
+
 from juba import models
 
 
-# @register(models.Branch)
-# class BranchTranslationOptions(TranslationOptions):
+@register(models.Services)
+class ServicesTranslationOptions(TranslationOptions):
+    fields = (
+        "title", "short_description", "detail_title", "detail_description", "detail_sub_title",
+        "detail_sub_description")
+
+
+@register(models.ServiceType)
+class ServiceTypeTranslationOptions(TranslationOptions):
+    fields = ("title", "description")
+
+
+@register(models.ServiceDetailSolo)
+class ServiceDetailSoloTranslationOptions(TranslationOptions):
+    fields = ("description",)
+
+
+@register(models.Works)
+class WorksTranslationOptions(TranslationOptions):
+    fields = ("short_description",)
+
+# @register(models.)
